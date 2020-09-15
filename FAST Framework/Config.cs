5fa265f9425e9133e291d742551e4cd63fecfdc6
tvoskoboinik <71014693+tvoskoboinik@users.noreply.github.com>
2020-09-15 11:58:38 -0300

    Add files via upload

---
+++ FAST Framework/Config.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FAST_Framework
+{
+    public static class Config
+    {
+
+        public static string MCPath = @"C:\Program Files (x86)\DSI\Mobile Client\DSI.MobileClient.PC.exe";
+        public static string MCProcName = "DSI.MobileClient.PC";
+        public static string XLPath = @"Z:\Teo\Prog\C#\FAST-Framework-master\FAST Framework\TestCases.xlsx";
+        public static class Credentials
+        {
+
+            public static class Valid
+            {
+                public static string Username = "matias.dotta@ARCHROCK.com";
+                public static string Password = "1Password!";
+                public static string RepeatPassword = "";
+                public static string Email = "";
+
+            }
+
+        }
+
+    }
+}
